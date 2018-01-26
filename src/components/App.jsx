3734a9d06cdc6e3b65668be3b6d578c81fefa2e8
Covalence Projects <paytonhaisten@gmail.com>
2018-01-26 12:44:32 -0600

    Phase Three Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FilmList from "./FilmList";
 import Jumbotron from "./Jumbotron";
+import PeopleList from "./PeopleList";
 import "../App.css";
 
 class App extends Component {
@@ -8,15 +9,24 @@ class App extends Component {
     super(props);
 
     this.state = {
-      hasLoaded: false
+      hasLoaded: false,
+      isFilmsLoaded: false,
+      isPeopleLoaded: false
     };
   }
-  handleToggle(event) {
-    const currentState = this.state.hasLoaded;
-    this.setState({ hasLoaded: !currentState });
+  handleTogglePeople(event) {
+    const currentStatePeople = this.state.isPeopleLoaded;
+    this.setState({ isPeopleLoaded: !currentStatePeople });
+    this.setState({ isFilmsLoaded: false });
   }
+  handleToggleFilms(event) {
+    const currentStateFilms = this.state.isFilmsLoaded;
+    this.setState({ isFilmsLoaded: !currentStateFilms });
+    this.setState({ isPeopleLoaded: false });
+  }
+
   render() {
-    if (this.state.hasLoaded === true) {
+    if (this.state.isFilmsLoaded === true) {
       return (
         <div>
           <Jumbotron />
@@ -24,15 +34,88 @@ class App extends Component {
             <button
               type="button"
               className="btn btn-primary my-auto"
+              id="film-button"
               data-toggle="button"
               aria-pressed="false"
               autoComplete="on"
-              onClick={event => this.handleToggle(event.target.value)}
+              onClick={event => this.handleToggleFilms(event.target.value)}
             >
-              Load
+              Load Films
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary my-auto"
+              id="people-button"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="on"
+              onClick={event => this.handleTogglePeople(event.target.value)}
+            >
+              Load People
             </button>
           </div>
           <FilmList />
+        </div>
+      );
+    } else if (this.state.isPeopleLoaded === true) {
+      return (
+        <div>
+          <Jumbotron />
+          <div className="button-container">
+            <button
+              type="button"
+              className="btn btn-primary my-auto"
+              id="film-button"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="on"
+              onClick={event => this.handleToggleFilms(event.target.value)}
+            >
+              Load Films
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary my-auto"
+              id="people-button"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="on"
+              onClick={event => this.handleTogglePeople(event.target.value)}
+            >
+              Load People
+            </button>
+          </div>
+          <PeopleList />
+        </div>
+      );
+    } else if (this.state.hasLoaded === false) {
+      return (
+        <div>
+          <Jumbotron />
+          <div className="button-container">
+            <button
+              type="button"
+              className="btn btn-primary my-auto"
+              id="film-button"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="on"
+              onClick={event => this.handleToggleFilms(event.target.value)}
+            >
+              Load Films
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary my-auto"
+              id="people-button"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="on"
+              onClick={event => this.handleTogglePeople(event.target.value)}
+            >
+              Load People
+            </button>
+          </div>
         </div>
       );
     } else {
@@ -43,12 +126,24 @@ class App extends Component {
             <button
               type="button"
               className="btn btn-primary my-auto"
+              id="film-button"
               data-toggle="button"
               aria-pressed="false"
               autoComplete="on"
-              onClick={event => this.handleToggle(event.target.value)}
+              onClick={event => this.handleToggleFilms(event.target.value)}
             >
-              Load
+              Load Films
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary my-auto"
+              id="people-button"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="on"
+              onClick={event => this.handleTogglePeople(event.target.value)}
+            >
+              Load People
             </button>
           </div>
         </div>
